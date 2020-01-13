8ec49a8af89fe25462233ba9e0851d4640ff4928
sjkrol <sjkro1@student.monash.edu>
2020-01-13 17:13:21 +0000

    Initial Commit

---
+++ server.R
@@ -0,0 +1,17 @@
+
+# Following script defines interactions between components displayed 
+# on the webpage.
+# 
+# This script belongs to the decision support system for preserving
+# digital files built by the University of Warwick and The National
+# Archive.
+# 
+# @author: Stephen James Krol, University of Monash, Melbourne
+# @email: stephen.james.krol@gmail.com
+
+library(shiny)
+
+shinyServer(function(input, output) {
+
+
+})
---
+++ ui.R
@@ -0,0 +1,15 @@
+
+# Following script defines components displayed on the webpage.
+# 
+# This script belongs to the decision support system for preserving
+# digital files built by the University of Warwick and The National
+# Archive.
+# 
+# @author: Stephen James Krol, University of Monash, Melbourne
+# @email: stephen.james.krol@gmail.com
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+))
