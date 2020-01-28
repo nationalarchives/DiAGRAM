3f55cd98327dbf2ba1cd290e8b49c339c87f6d34
sjkrol <sjkro1@student.monash.edu>
2020-01-28 14:35:27 +0000

    Updated to version 0.6.0

--- ui.R
+++ ui.R
@@ -79,7 +79,7 @@ dashboardPage(
           shinydashboard::box(
             title = "Welcome",
             width = 12,
-            shiny::h2("Version 0.5.0"),
+            shiny::h2("Version 0.6.0"),
             br(),
             p("This an initial version of the decision support system
               built by the ",
