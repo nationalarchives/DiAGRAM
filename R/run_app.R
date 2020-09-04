9c902ce9e275f70e1796d26d595c8e2981d582c8
Theo Roe <theo@jumpingrivers.com>
2020-09-04 17:01:00 +0100

    added run_app function

---
+++ R/run_app.R
@@ -0,0 +1,13 @@
+#' Run the diagram app
+#'
+#' Runs the diagram app using the partial function from purrr.
+#'
+#' @param n Number of questions, defaults to 9
+#' @importFrom purrr partial
+#' @importFrom shiny shinyApp
+#' @export
+run_app = function(n = 9){
+#  browser()
+  app_ui_partial = purrr::partial(diagram::app_ui, nquestions = n)
+  shiny::shinyApp(ui = app_ui_partial, server = diagram::app_server)
+}
