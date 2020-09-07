e0a50c32550af00dc3ba08176170c22aca4be535
Colin Gillespie <csgillespie@gmail.com>
2020-09-07 13:52:29 +0100

    Deploy to shinyapps

--- deploy_to_shinyapps.R
+++ deploy_to_shinyapps.R
@@ -2,6 +2,14 @@ if (!requireNamespace("rsconnect")) install.packages("rsconnect")
 if (!requireNamespace("stringr")) install.packages("stringr")
 if (!requireNamespace("cli")) install.packages("cli")
 
+install_pkg = function() {
+  cli::cli_h1("Install pkg")
+  path = paste(Sys.getenv('TRAVIS_REPO_SLUG'), Sys.getenv("TRAVIS_BRANCH"), sep = "@")
+  cli::cli_alert_info("Installing {path}")
+  remotes::install_github(path)
+  cli::cli_alert_success("{path} installed!")
+}
+
 deploy = function(account = "jumpingrivers", server = "shinyapps.io") {
   cli::cli_h1("Deploying app")
   rsconnect::setAccountInfo(name = account,
@@ -28,7 +36,7 @@ terminate = function(account = "jumpingrivers", server = "shinyapps.io") {
 }
 
 
-
+install_pkg()
 deploy()
 terminate()
 
