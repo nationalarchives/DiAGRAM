8f6964ae68b58201fec2f54d3a3a1e4a8dcc623e
archangel <hannah.merwood@nationalarchives.gov.uk>
2021-02-22 12:32:31 +0000

    text and video placeholder

--- R/guidance_tab.R
+++ R/guidance_tab.R
@@ -7,6 +7,12 @@ guidance_tab = function() {
     shinydashboard::box(
       width = 12,
       shiny::includeMarkdown(system.file("text_content", "guidance_extra.md", package = "diagramNAT"))
+    ),
+    shinydashboard::box(
+      width = 12,
+      shiny::h4("Video of DiAGRAM in use"),
+      shiny::p("This is a recording of a live demonstration of how to use the tool. At the time of recording it was not anticipated that this would be shared as part of the DiAGRAM guidance, so please do not expect it to be a copmrehensive walkthrough guide. However, we hope you may find it useful."),
+      shiny::tags$video(id="video", type = "video/mp4",src = "Diagramdemo_Paper_Archive_EDIT_1-2-2021.mp4", controls = "controls")
     )
   )
 }
