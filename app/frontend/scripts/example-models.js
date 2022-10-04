593b65760e74fe689b95585fd07686cff5182920
Jack Walton <jack@jumpingrivers.com>
2022-10-04 14:20:37 +0100

    fix: Update placeholder values in reference model 'Commercial backup'

--- app/frontend/scripts/example-models.js
+++ app/frontend/scripts/example-models.js
@@ -6,43 +6,49 @@
 		{
 			model_name: 'Example - Commercial Backup',
 			scenario: 'Base Model',
-			notes: 'This archive that has no knowledge of digital preservation but outsources their data storage of their born-digital records. They have an inventory of content but nothing more and have not thought about preserving or rendering content, only storing the bit-stream.',
+			notes: 'The archive has no knowledge of digital preservation but outsources their data storage of their born-digital records. They have an inventory of content but nothing more and have not thought about preserving or rendering content, only storing the bit-stream.',
 			is_advanced: false,
-			intellectual_control: 92,
-			renderability: 80,
+			intellectual_control: 6,
+			renderability: 38,
 			response: {
-				Op_Environment: {
-					1: 0,
-					2: 'Not Applicable - we have copies offsite'
-				},
-				Info_Management: {
-					1: 'Not achieved',
-					2: 'Not achieved',
-					3: [
-						'Minimal awareness',
-						'Minimal awareness'
-					]
-				},
-				Rep_and_Refresh: {
-					1: 100,
-					2: 100
-				},
-				System_Security: {
-					1: 'ISO 27001',
-					2: 'None, or only minor issues outstanding',
-					3: 'Level 4',
-					4: 'Yes'
-				},
 				Digital_Object: [
+					100,
+					0,
+					0
+				],
+				Storage_Medium: [
 					0,
 					0,
 					100
 				],
+				Rep_and_Refresh: {
+					1: 100,
+					2: 100
+				},
+				Op_Environment: {
+					1: 100,
+					2: 'Not Applicable - we have copies offsite'
+				},
+				Physical_Disaster: Very Low,
 				Checksum: [
 					0,
-					9,
-					91
+					0,
+					100
 				],
+				System_Security: {
+					1: 'ISO 27001',
+					2: 'No test',
+					3: 'Level 2',
+					4: 'No'
+				},
+				Info_Management: {
+					1: 'Level 1',
+					2: 'Not achieved',
+					3: [
+						'Minimal awareness',
+						'Awareness'
+					]
+				},
 				Technical_Skills: [
 					'None',
 					'None',
@@ -54,13 +60,7 @@
 					'None',
 					'None',
 					'None'
-				],
-				Storage_Medium: [
-					0,
-					0,
-					100
-				],
-				Physical_Disaster: 'Very Low'
+				]
 			},
 			advanced: null
 		},
@@ -111,17 +111,17 @@
 					]
 				},
 				Technical_Skills: [
-				 'Advanced',
-				 'Advanced',
-				 'Advanced',
-				 'Advanced',
-				 'Advanced',
-				 'Advanced',
-				 'Advanced',
-				 'Advanced',
-				 'Advanced',
-				 'Advanced'
-				]
+					'Advanced',
+					'Advanced',
+					'Advanced',
+					'Advanced',
+					'Advanced',
+					'Advanced',
+					'Advanced',
+					'Advanced',
+					'Advanced',
+					'Advanced'
+			    ]
 			},
 			advanced: null
 		},
