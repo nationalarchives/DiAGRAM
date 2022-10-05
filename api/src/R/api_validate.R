9d6391f1c0116fad86a3646794ffc45ea48effab
Jack Walton <jack@jumpingrivers.com>
2022-10-05 12:09:31 +0100

    fix: Serialise data returned by validate_json()

--- api/src/R/api_validate.R
+++ api/src/R/api_validate.R
@@ -8,12 +8,6 @@
 validate_json = function(req) {
   ensure_json_input(req)
   parsed_json = jsonlite::parse_json(req$postBody)
-  if (!is_array(parsed_json)) {
-    # if we only have a single entity box it into an array
-    parsed_json = list(parsed_json)
-  }
   valid = validate(parsed_json)
-  list(
-    "status" = valid
-  )
+  serialise_r(list("status" = valid))
 }
