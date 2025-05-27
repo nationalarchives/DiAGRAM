6f0132f29add21f9499fb6c66ce297e65b0404d7
sparkhi <4743002+sparkhi@users.noreply.github.com>
2025-05-27 17:22:13 +0100

    run e2e tests - work in progress

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -1,8 +1,12 @@
 const { defineConfig } = require("cypress");
 
+const defaultEnvUrl = `https://dev-diagram.nationalarchives.gov.uk`
+const envPrefix = process.env.environmentPrefix || ''
+const executionEnvUrl = envPrefix ? `https://${envPrefix}-diagram.nationalarchives.gov.uk` : defaultEnvUrl
+
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://dev-diagram.nationalarchives.gov.uk",
+    baseUrl: executionEnvUrl,
     supportFile: false,
   },
 });
